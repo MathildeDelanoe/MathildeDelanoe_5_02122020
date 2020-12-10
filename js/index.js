6533c73316f23eb0f8510e011f0a2f73996201f0
Mathilde Delanoe <openclassrooms@dlne.eu>
2020-12-10 15:03:49 +0100

    Creation des fonctions fillCatalogue et addToCatalogue pour remplir le catalogue Pour le moment, le contenu n'est pas rempli. Seul l'architecture est présente.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+const PRODUCTS_NUMBER_PER_ROW = 3;
+const BOOTSTRAP_COLUMNS_NUMBER = 12;
+const BOOTSTRAP_COLUMNS_PER_PRODUCT = BOOTSTRAP_COLUMNS_NUMBER / PRODUCTS_NUMBER_PER_ROW;
+
+function addToCatalogue(product)
+{
+    var createdColumn = document.createElement("div");
+    createdColumn.classList.add("col"); // Creation de la colonne avec une classe "col"
+    return createdColumn;
+}
+
+function fillCatalogue(productsTable)
+{
+    let container = document.querySelector("div.container-fluid");
+    let productsCounter = 0;
+    for (let product of productsTable)
+    {
+        if ((productsCounter % PRODUCTS_NUMBER_PER_ROW) === 0)
+        {
+            var currentRow = document.createElement("div");
+            currentRow.classList.add("row"); // Creation de la ligne avec une classe "row"
+            container.appendChild(currentRow); // Ajout de la ligne au container
+        }
+        let currentColumn = addToCatalogue(product);
+        currentRow.appendChild(currentColumn);
+        productsCounter++;
+    }
+}
+
 // Interrogation du serveur via une requete HTTP en utilisant l'API fetch
 fetch("http://localhost:3000/api/teddies/")
 .then(function(response)
@@ -13,6 +42,6 @@ fetch("http://localhost:3000/api/teddies/")
 })
 .then(function(response)
 {
-    console.log(response);
+    fillCatalogue(response);
 })
 .catch(error => console.log("Erreur : " + error))
